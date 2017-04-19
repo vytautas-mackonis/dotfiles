f9050533ae3c8359ad905e970d00c852bb06b399
vytautas.mackonis <vytautas.mackonis@gmail.com>
2017-04-19 20:38:19 +0300

    minor fixes

--- linux/docker-ubuntu.sh
+++ linux/docker-ubuntu.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
 sudo usermod -aG docker $(whoami)
