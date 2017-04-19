f0e0459794b207a5d2e26aba7546fdba00ea7bb4
vytautas.mackonis <vytautas.mackonis@gmail.com>
2017-04-20 00:26:12 +0300

    reverting to vanilla omnisharp because roslyn one is broken for now

--- vim/install.sh
+++ vim/install.sh
@@ -8,20 +8,16 @@ mkdir -p ~/.vim/swapfiles
 if [ ! -f ~/.vim/autoload/plug.vim ]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
             https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    ln -s $DOTFILES/vim/vimrc $HOME/.vim/vimrc
 fi
 vim +PlugInstall +qall
 
-#vimproc plugin needs to be compiled after installation
-#pushd ~/.vim/bundle/vimproc.vim
-#make
-#popd
+omnisharp compilation
+pushd ~/.vim/bundle/omnisharp-vim/server
+xbuild
+popd
 
-#omnisharp compilation
-#pushd ~/.vim/bundle/omnisharp-vim/server
-#xbuild
-#popd
-
-#pushd ~/.vim/bundle/omnisharp-vim/omnisharp-roslyn
-#git checkout dev
-#./build.sh
-#popd
+pushd ~/.vim/bundle/omnisharp-vim/omnisharp-roslyn
+git checkout dev
+./build.sh
+popd
